633ebd105e9760d44190b5c6923e3105943a764b
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-19 20:29:15 -0700

    Added basic login and register pages

--- client/src/features/login/Login.jsx
+++ client/src/features/login/Login.jsx
@@ -1,16 +1,113 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-
 import axios from 'axios';
 
+// import { useAuth } from '../context/AuthContext';
+// import { hybridEncrypt, loadPublicKeyFromPemUrl } from '../utils/EncryptPayload';
+import API_BASE_URL from '../shared/utils/api';
+
+import './login.css';
+
 const Login = () => {
-    
+    // const { setUser, checkAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const registrationSuccess = location.state?.registrationSuccess;
+    const passwordChanged = location.state?.passwordChanged;
+    const [fadeOut, setFadeOut] = useState(false); // for success popup
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    // const [publicKey, setPublicKey] = useState(null);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            // const key = publicKey || await loadPublicKeyFromPemUrl('/keys/public_key.pem');
+            // setPublicKey(key);
+            // const encryptedPayload = await hybridEncrypt(key, { email, password });
+
+            const res = await axios.post(
+                `${API_BASE_URL}/auth/login`,
+                { email, password }
+                // { withCredentials: true }
+            );
+
+            setUser(res.data.user);
+            navigate('/dashboard');
+            checkAuth();
+        } catch (err) {
+            const serverError = err.response?.data?.error || "Unknown error occurred during login";
+            setError(`Login failed: ${serverError}`);
+        }
+    };
+
+    const handleRegisterRedirect = (e) => {
+        e.preventDefault();
+        navigate('/register');
+    };
+
+    useEffect(() => {
+        if (registrationSuccess || passwordChanged) {
+            const fadeTimer = setTimeout(() => setFadeOut(true), 4000);
+            const removeTimer = setTimeout(() => {
+                navigate(location.pathname, { replace: true, state: {} });
+            }, 5000);
+
+            return () => {
+                clearTimeout(fadeTimer);
+                clearTimeout(removeTimer);
+            };
+        }
+    }, [registrationSuccess, passwordChanged, location.pathname, navigate]);
 
     return (
-        <>
-        
-        </>
-    )
-}
+        <div className="login-page">
+            {registrationSuccess && (
+                <div className={`popup-success ${fadeOut ? 'fade-out' : ''}`}>
+                    Click the verification link in your email to finish registration
+                </div>
+            )}
+            {passwordChanged && (
+                <div className={`popup-success ${fadeOut ? 'fade-out' : ''}`}>
+                    Password changed successfully. Please log in with the new password
+                </div>
+            )}
+            <form onSubmit={handleLogin} className="login-form">
+                <h2>Welcome Back</h2>
+                {error && <p className="message-error">{error}</p>}
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    className="general-input-1"
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    className="general-input-1"
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+                <button className="general-button-1" type="submit">Login</button>
+
+                <p className="register-text">
+                    Don’t have an account?{' '}
+                    <span className="register-link" onClick={handleRegisterRedirect}>
+                        Register
+                    </span>
+                </p>
+
+            </form>
+        </div>
+    );
+};
 
 export default Login;
--- client/src/features/register/Register.jsx
+++ client/src/features/register/Register.jsx
@@ -1,16 +1,87 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
+// import { hybridEncrypt, loadPublicKeyFromPemUrl } from '../utils/EncryptPayload';
+import API_BASE_URL from '../shared/utils/api';
+
+import './register.css';
+
 const Register = () => {
-    
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const navigate = useNavigate();
+
+    const [error, setError] = useState('');
+
+    const [publicKey, setPublicKey] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+
+        try {
+            const key = publicKey || await loadPublicKeyFromPemUrl('/keys/public_key.pem');
+            setPublicKey(key);
+
+            const { confirmPassword, ...formDataToSend } = formData; // exclude confirmPassword
+            const encryptedPayload = await hybridEncrypt(key, {
+                ...formDataToSend
+            });
+            
+            await axios.post(
+                `${API_BASE_URL}/auth/register`,
+                { ...encryptedPayload }
+            );
+            navigate('/', { state: { registrationSuccess: true } });
+        } catch (err) {
+            const serverError = err.response?.data?.error || "Unknown error occurred during registration";
+            setError(`Registration failed: ${serverError}`);
+        }
+    };
+
+    const handleLoginRedirect = (e) => {
+        e.preventDefault();
+        navigate('/');
+    };
 
     return (
-        <>
-        
-        </>
-    )
-}
+        <div className="register-page">
+            <form onSubmit={handleRegister} className="register-form">
+                <h2>Create Account</h2>
+                {error && <p className="error">{error}</p>}
+
+                <input type="text" name="firstname" placeholder="First Name" value={formData.firstname} className="general-input-1" onChange={handleChange} required />
+                <input type="text" name="lastname" placeholder="Last Name" value={formData.lastname} className="general-input-1" onChange={handleChange} required />
+                <input type="email" name="email" placeholder="Email" value={formData.email} className="general-input-1" onChange={handleChange} required />
+                <input type="password" name="password" placeholder="Password" value={formData.password} className="general-input-1" onChange={handleChange} required />
+                <input type="password" name="confirmPassword" placeholder="Retype Password" value={formData.confirmPassword} className="general-input-1" onChange={handleChange} required />
+
+                <button type="submit" className="general-button-1">Register</button>
+
+                <p className="login-text">
+                    Already have an account?{' '}
+                    <span className="login-link" onClick={handleLoginRedirect}>
+                        Login
+                    </span>
+                </p>
+            </form>
+        </div>
+    );
+};
 
 export default Register;
