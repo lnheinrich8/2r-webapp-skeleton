b3da19e38c3f29c846a5cb25bc142e541a5fcb94
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-23 22:20:53 -0700

    Added empty settings modal

---
+++ client/src/features/shared/components/settings-modal/SettingsModal.jsx
@@ -0,0 +1,13 @@
+// Styling
+import './settings-modal.css';
+
+const SettingsModal = () => {
+
+    return (
+        <div className="settings-modal-main">
+            
+        </div>
+    );
+}
+
+export default SettingsModal;
