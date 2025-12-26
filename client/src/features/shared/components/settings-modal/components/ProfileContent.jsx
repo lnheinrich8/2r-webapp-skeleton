b7a511734f8967866596059a5c40b75d945bca61
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-26 11:56:50 -0700

    Scrollable settings modal

--- client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
+++ client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
@@ -1,14 +1,18 @@
 import { useAuth } from '../../../../auth/AuthContext';
 
+// Styling
+import './profilecontent.css'
+
 const ProfileContent = () => {
     const { user, loading } = useAuth();
-    
+
 
     if (loading || !user) return null;
 
     return (
-        <>
-        </>
+        <div className="profile-content-main">
+            
+        </div>
     )
 }
 
