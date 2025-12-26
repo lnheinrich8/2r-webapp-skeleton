003fbacfea7bef66c025360eaca44a4e1ee07b65
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-25 22:51:42 -0700

    Breh

--- client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
+++ client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
@@ -2,8 +2,8 @@ import { useAuth } from '../../../../auth/AuthContext';
 
 const ProfileContent = () => {
     const { user, loading } = useAuth();
-
     
+
     if (loading || !user) return null;
 
     return (
