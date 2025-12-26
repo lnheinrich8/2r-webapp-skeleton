e94583ee9df1418b4064e71acc1700ba0b657cbe
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-26 14:02:52 -0700

    Added section buttons and content

--- client/src/features/shared/components/settings-modal/SettingsModal.jsx
+++ client/src/features/shared/components/settings-modal/SettingsModal.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
+// Components
+import ProfileContent from './components/ProfileContent';
+import DataContent from './components/DataContent';
+
 // Styling
 import './settings-modal.css';
 
+const sections = [
+    { id: 'profile', label: 'Profile', component: ProfileContent },
+    { id: 'data', label: 'Data', component: DataContent },
+];
+
 const SettingsModal = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
+
+    // Showing content based on active section clicked (default profile content)
+    const [activeSection, setActiveSection] = useState('profile');
+    const ActiveComponent = sections.find(section => section.id === activeSection)?.component ?? ProfileContent;
 
     const handleOverlayMouseDown = (event) => {
         if (event.target === event.currentTarget) {
@@ -21,17 +36,24 @@ const SettingsModal = ({ isOpen, onClose }) => {
                 <hr className="sm-horizontal-divider" />
 
                 <div className="sm-section-content-container">
-                    
                     <div className="sm-sections-container">
-
+                        {sections.map(section => (
+                            <button
+                                key={section.id}
+                                type="button"
+                                className={`sm-section-button ${activeSection === section.id ? 'active' : ''}`}
+                                onClick={() => setActiveSection(section.id)}
+                            >
+                                {section.label}
+                            </button>
+                        ))}
                     </div>
 
-                    <vr className="sm-vertical-divider" />
+                    <div className="sm-vertical-divider" />
 
                     <div className="sm-content-container">
-
+                        <ActiveComponent />
                     </div>
-                    
                 </div>
             </div>
         </div>
