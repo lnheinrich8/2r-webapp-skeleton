08ad645c5e1892ce4acdebe17bcf4cbf18922b14
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-27 21:37:39 -0700

    Added some buttons in profile content in sm

--- client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
+++ client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
@@ -11,7 +11,18 @@ const ProfileContent = () => {
 
     return (
         <div className="sm-content-main">
-            
+
+            <h className="sm-content-header">Edit</h>
+
+            <div className="sm-content-row">
+                <span className="sm-content-label">Edit profile</span>
+                <button className="sm-content-button">Edit</button>
+            </div>
+
+            <hr className="sm-content-horizontal-divider" />
+
+            <h className="sm-content-header">Info</h>
+
             <div className="sm-content-row">
                 <span className="sm-content-label">First Name</span>
                 <span className="sm-content-value">{user.firstname}</span>
@@ -21,12 +32,26 @@ const ProfileContent = () => {
                 <span className="sm-content-label">Last Name</span>
                 <span className="sm-content-value">{user.lastname}</span>
             </div>
-            
+
             <div className="sm-content-row">
                 <span className="sm-content-label">Email</span>
                 <span className="sm-content-value">{user.email}</span>
             </div>
-        
+
+            <hr className="sm-content-horizontal-divider" />
+
+            <h className="sm-content-header">Security</h>
+
+            <div className="sm-content-row">
+                <span className="sm-content-label">Change password</span>
+                <button className="sm-content-button-red">Change</button>
+            </div>
+
+            <div className="sm-content-row">
+                <span className="sm-content-label">Delete account</span>
+                <button className="sm-content-button-red">Delete</button>
+            </div>
+
         </div>
     )
 }
