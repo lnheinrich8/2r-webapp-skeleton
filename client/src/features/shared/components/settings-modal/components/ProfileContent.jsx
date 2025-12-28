5a6edaad1de637ecd747215a80f15f0ba337a5ea
Luke Heinrich <lnheinrich8@gmail.com>
2025-12-27 22:26:27 -0700

    User info editing inputs in sm

--- client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
+++ client/src/features/shared/components/settings-modal/components/ProfileContent.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAuth } from '../../../../auth/AuthContext';
 
 // Styling
@@ -6,41 +7,85 @@ import './smcontent.css'
 const ProfileContent = () => {
     const { user, loading } = useAuth();
 
+    const [isEditing, setIsEditing] = useState(false);
+
+
+    const handleEditToggle = () => {
+        if (!isEditing) {
+            setIsEditing(true);
+            return;
+        }
+
+        // Exit edit mode
+        setIsEditing(false);
+    }
+
+
+
 
     if (loading || !user) return null;
 
     return (
         <div className="sm-content-main">
 
-            <h className="sm-content-header">Edit</h>
+            <p className="sm-content-header">Edit</p>
 
             <div className="sm-content-row">
                 <span className="sm-content-label">Edit profile</span>
-                <button className="sm-content-button">Edit</button>
+                <button className="sm-content-button" onClick={handleEditToggle}>
+                    {isEditing ? 'Save' : 'Edit'}
+                </button>
             </div>
 
             <hr className="sm-content-horizontal-divider" />
 
-            <h className="sm-content-header">Info</h>
+            <p className="sm-content-header">Info</p>
 
             <div className="sm-content-row">
                 <span className="sm-content-label">First Name</span>
-                <span className="sm-content-value">{user.firstname}</span>
+
+                <span className="sm-content-value">
+                    {isEditing ? (
+                        <input
+                            className="sm-content-edit-input"
+                            type="firstname"
+                            value={user.firstname}
+                        />
+                    ) : user.firstname}
+                </span>
             </div>
 
             <div className="sm-content-row">
                 <span className="sm-content-label">Last Name</span>
-                <span className="sm-content-value">{user.lastname}</span>
+
+                <span className="sm-content-value">
+                    {isEditing ? (
+                        <input
+                            className="sm-content-edit-input"
+                            type="lastname"
+                            value={user.lastname}
+                        />
+                    ) : user.lastname}
+                </span>
             </div>
 
             <div className="sm-content-row">
                 <span className="sm-content-label">Email</span>
-                <span className="sm-content-value">{user.email}</span>
+
+                <span className="sm-content-value">
+                    {isEditing ? (
+                        <input
+                            className="sm-content-edit-input"
+                            type="email"
+                            value={user.email}
+                        />
+                    ) : user.email}
+                </span>
             </div>
 
             <hr className="sm-content-horizontal-divider" />
 
-            <h className="sm-content-header">Security</h>
+            <p className="sm-content-header">Security</p>
 
             <div className="sm-content-row">
                 <span className="sm-content-label">Change password</span>
